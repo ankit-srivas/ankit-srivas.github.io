af536eb65caadccb96f0dd3c8f4d04081714d19b
ankit-srivas <ascontinentalgt@gmail.com>
2024-02-15 18:09:08 -0500

    Updated index.html

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Form submission handling
-    const form = document.querySelector('form');
-
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-
-        // Get form data
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('message').value;
-
-        // Log form data to the console
-        console.log('Form Data:');
-        console.log('Name:', name);
-        console.log('Email:', email);
-        console.log('Message:', message);
-
-    });
-});
+var id = null;
+function myMove() {
+  var elem = document.getElementById("myAnimation");
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
